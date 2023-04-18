1c143eb0e8d8bded5b4d0498c59f491b414e59cc
zvz23 <ziegfredzorrilla23@gmail.com>
2023-04-18 11:00:24 +0800

    fixex limit in api call

--- vProfanity/Services/PerspectiveAPI.cs
+++ vProfanity/Services/PerspectiveAPI.cs
@@ -26,27 +26,94 @@ namespace vProfanity.Services
 
         public async Task<ScoreResponse> AnaylizeText(string text)
         {
-            string rawJson = JsonConvert.SerializeObject(new
+            ScoreResponse averageScoreResponse = new ScoreResponse()
             {
-                comment = new
+                attributeScores = new AttirubteScores()
                 {
-                    text = text
-                },
-                languages = new string[] { "en" },
-                requestedAttributes = new
-                {
-                    TOXICITY = new {},
-                    IDENTITY_ATTACK = new {},
-                    INSULT = new {},
-                    PROFANITY = new {},
-                    THREAT = new {}
+                    TOXICITY = new Attribute()
+                    {
+                        summaryScore = new SummaryScore()
+                    },
+                    IDENTITY_ATTACK = new Attribute()
+                    {
+                        summaryScore = new SummaryScore()
+                    },
+                    INSULT = new Attribute() 
+                    { 
+                        summaryScore = new SummaryScore() 
+                    },
+                    PROFANITY = new Attribute() 
+                    { 
+                        summaryScore = new SummaryScore() 
+                    },
+                    THREAT = new Attribute() 
+                    { 
+                        summaryScore = new SummaryScore() 
+                    }
                 }
-            });
-            var jsonContent = new StringContent(rawJson,Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await _httpClient.PostAsync("", jsonContent);
-            string json = await response.Content.ReadAsStringAsync();
-            ScoreResponse scoreResponse = JsonConvert.DeserializeObject<ScoreResponse>(json);
-            return scoreResponse;
+            };
+            List<ScoreResponse> scoreResponses = new List<ScoreResponse>();
+            List<string> textSegments = SplitText(text, 18000);
+            foreach (var segment in textSegments)
+            {
+                string rawJson = JsonConvert.SerializeObject(new
+                {
+                    comment = new
+                    {
+                        text = segment
+                    },
+                    languages = new string[] { "en" },
+                    requestedAttributes = new
+                    {
+                        TOXICITY = new { },
+                        IDENTITY_ATTACK = new { },
+                        INSULT = new { },
+                        PROFANITY = new { },
+                        THREAT = new { }
+                    }
+                });
+                var jsonContent = new StringContent(rawJson, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _httpClient.PostAsync("", jsonContent);
+                string json = await response.Content.ReadAsStringAsync();
+                ScoreResponse scoreResponse = JsonConvert.DeserializeObject<ScoreResponse>(json);
+                scoreResponses.Add(scoreResponse);
+            }
+            averageScoreResponse.attributeScores.TOXICITY.summaryScore.value = scoreResponses.Select(sr => sr.attributeScores.TOXICITY.summaryScore.value).Average();
+            averageScoreResponse.attributeScores.IDENTITY_ATTACK.summaryScore.value = scoreResponses.Select(sr => sr.attributeScores.IDENTITY_ATTACK.summaryScore.value).Average();
+            averageScoreResponse.attributeScores.INSULT.summaryScore.value = scoreResponses.Select(sr => sr.attributeScores.INSULT.summaryScore.value).Average();
+            averageScoreResponse.attributeScores.PROFANITY.summaryScore.value = scoreResponses.Select(sr => sr.attributeScores.PROFANITY.summaryScore.value).Average();
+            averageScoreResponse.attributeScores.THREAT.summaryScore.value = scoreResponses.Select(sr => sr.attributeScores.THREAT.summaryScore.value).Average();
+
+
+            return averageScoreResponse;
+
+
+        }
+
+        public List<string> SplitText(string text, int chunkSize)
+        {
+            var chunks = new List<string>();
+
+            var words = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var currentChunk = new StringBuilder();
+            foreach (var word in words)
+            {
+                if (currentChunk.Length + word.Length + 1 > chunkSize)
+                {
+                    chunks.Add(currentChunk.ToString().Trim());
+                    currentChunk.Clear();
+                }
+
+                currentChunk.Append(word + " ");
+            }
+
+            if (currentChunk.Length > 0)
+            {
+                chunks.Add(currentChunk.ToString().Trim());
+            }
+
+            return chunks;
         }
 
 
@@ -54,7 +121,7 @@ namespace vProfanity.Services
 
     public class SummaryScore
     {
-        public string value { get; set; }
+        public double value { get; set; }
         public string type { get; set; }
     }
 
