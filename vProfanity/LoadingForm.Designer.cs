2f591e5623445ced566c10047dd9c6cf0ab6756d
zvz23 <ziegfredzorrilla23@gmail.com>
2023-05-10 18:25:37 +0800

    added loading form

---
+++ vProfanity/LoadingForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace vProfanity
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadingLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::vProfanity.Properties.Resources.loading1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 53);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadingLabel
+            // 
+            this.loadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadingLabel.Location = new System.Drawing.Point(88, 12);
+            this.loadingLabel.Name = "loadingLabel";
+            this.loadingLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.loadingLabel.Size = new System.Drawing.Size(230, 53);
+            this.loadingLabel.TabIndex = 1;
+            this.loadingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 77);
+            this.Controls.Add(this.loadingLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.LoadingForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label loadingLabel;
+    }
+}
---
+++ vProfanity/LoadingForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace vProfanity
+{
+    public partial class LoadingForm : Form
+    {
+        public string LoadingMessage { get; set; }
+        public LoadingForm()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void LoadingForm_Load(object sender, EventArgs e)
+        {
+            loadingLabel.Text = LoadingMessage;
+        }
+    }
+}
